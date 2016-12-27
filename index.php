701fe4b6f7ccc06108689eb3ad6b2cae571d2af0
CamoFPV <admin@camofpv.com>
2016-12-27 00:29:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<title>Raspberry Pi Relay Control</title>
+<head>
+<link rel="stylesheet" href="./assets/style.css" type="text/css" />
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="refresh" content="120" > 
+<body>
+<div class="container">
+     <?php require("./DATA/Relay2.html"); ?>
+     <?php require("./DATA/Relay11.html"); ?>
+     <a href="./allon.php"><input class="MyButtonON" value="All ON"></a>
+     <a href="./alloff.php"><input class="MyButton" value="All OFF"></a>
+</div>
+</br >
+<div class="container">
+     <?php include("./DATA/Relay3.html"); ?>
+     <?php include("./DATA/Relay5.html"); ?>
+</div>
+</br >
+<div class="container">
+     <?php include("./DATA/Relay4.html"); ?>
+     <?php include("./DATA/Relay6.html"); ?>
+</div>
+</br >
+<div class="container">
+     <?php include("./DATA/Relay17.html"); ?>
+     <?php include("./DATA/Relay13.html"); ?>
+</div>
+</br >
+<div class="container">
+     <?php include("./DATA/Relay27.html"); ?>
+     <?php include("./DATA/Relay19.html"); ?>
+</div>
+</br >
+<div id="body" class="container">
+     <?php include("./DATA/Relay22.html"); ?>
+     <?php include("./DATA/Relay26.html"); ?>
+</div>
+</br >
+<div id="body" class="container">
+     <?php include("./DATA/Relay10.html"); ?>
+     <?php include("./DATA/Relay21.html"); ?>
+</div>
+</br >
+<div id="body" class="container">
+     <?php require("./DATA/Relay9.html"); ?>
+     <?php require("./DATA/Relay20.html"); ?>
+</div>
+</body>
